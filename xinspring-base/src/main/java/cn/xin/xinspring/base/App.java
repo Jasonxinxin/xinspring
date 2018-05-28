55d407ecd49233c16faf5591bfedcc09141325aa
Jasonxinxin <www651285630@qq.com>
2018-05-29 00:21:48 +0800

    创建新spring，配置了最基础的依赖，给出的是一个项目的基础

---
+++ xinspring-base/src/main/java/cn/xin/xinspring/base/App.java
@@ -0,0 +1,13 @@
+package cn.xin.xinspring.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
